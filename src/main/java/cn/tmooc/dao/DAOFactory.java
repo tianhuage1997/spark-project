7d98a40d474d558d101556cda63f05fbc9d6b28a
tianhuage1997 <FENGtong521314>
2019-04-19 10:55:08 +0800

    Initial commit

---
+++ src/main/java/cn/tmooc/dao/DAOFactory.java
@@ -0,0 +1,21 @@
+package cn.tmooc.dao;
+
+
+import cn.tmooc.dao.impl.TaskDAOImpl;
+
+/**
+ * DAO工厂类
+ * @author Administrator
+ *
+ */
+public class DAOFactory {
+
+	/**
+	 * 获取任务管理DAO
+	 * @return DAO
+	 */
+	public static ITaskDAO getTaskDAO() {
+		return new TaskDAOImpl();
+	}
+	
+}
---
+++ src/main/java/cn/tmooc/test/TaskDAOTest.java
@@ -0,0 +1,21 @@
+package cn.tmooc.test;
+
+
+import cn.tmooc.dao.DAOFactory;
+import cn.tmooc.dao.ITaskDAO;
+import cn.tmooc.domain.Task;
+
+/**
+ * 任务管理DAO测试类
+ * @author Administrator
+ *
+ */
+public class TaskDAOTest {
+	
+	public static void main(String[] args) {
+		ITaskDAO taskDAO = DAOFactory.getTaskDAO();
+		Task task = taskDAO.findById(2);
+		System.out.println(task.getTaskName());  
+	}
+	
+}
